c90ea1811ef765e55a9193bb51199ada15e4a35f
Finskou <lelarhaz@gmail.com>
2018-09-22 22:34:11 +0200

    messages send correctly

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io').listen(server)
+var io = require('socket.io').listen(server);
 users = [];
 connections = [];
+
 //run server
 server.listen(process.env.PORT || 3000);
 console.log('server running')
@@ -12,20 +13,22 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html')
 });
 
+
 io.sockets.on('connection', function(socket){
 	//all events stay in here
 	connections.push(socket);
 	console.log('connected:%s sockets connected', connections.length);
 	
-
 	//Disconnect
 	socket.on('disconnect', function(data){
-	connections.splice(connections.indexOf(socket), 1)
+		console.log(data);
+	connections.splice(connections.indexOf(socket), 1);
 	console.log('Disconnected : %s sockets connected', connections.length);
 	});
 
 	//Send Message
 	socket.on('send message', function(data){
+		console.log(data)
 		io.sockets.emit('new message', {msg: data});
 	})
 	
