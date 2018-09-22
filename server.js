c42edd3afb8d345d2320983738da82c00877477f
Finskou <lelarhaz@gmail.com>
2018-09-22 21:57:16 +0200

    update jquery for sending messages

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ io.sockets.on('connection', function(socket){
 	socket.on('disconnect', function(data){
 	connections.splice(connections.indexOf(socket), 1)
 	console.log('Disconnected : %s sockets connected', connections.length);
-
 	});
+
+	//Send Message
+	socket.on('send message', function(data){
+		io.sockets.emit('new message', {msg: data});
+	})
 	
 })
